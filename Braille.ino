ac2580c853d734c3bcce6d0494c3029a5eb7972b
Manuel Borrajo <borrajo@hotmail.com.ar>
2016-11-06 22:33:04 -0300

    Init
    Primer commit

---
+++ Braille.ino
@@ -0,0 +1,110 @@
+  #include <SPI.h>
+  #include "Keyboard.h"
+ // #define DATAout 51
+ // #define CLK 52
+ // #define DATAin 50
+  #define SSB 4 //Select Slave Button
+  #define SSL 2 //Select Slave Led
+  #define BYTES 1 //Cantidad de bytes (botones y luces)*8 
+  #define KEY_BACKSPACE 178
+  #define KEY_RETURN 176
+  char charmap[256];
+  char nummap[256];
+ 
+void setup()
+{
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV8);
+  pinMode(SSB,OUTPUT);
+  pinMode(SSL,OUTPUT);
+  digitalWrite(SSB,HIGH);
+  digitalWrite(SSL,HIGH); 
+  Keyboard.begin();
+  iniciarmapa();
+}
+
+void loop()
+{
+  unsigned long l = leerSPI();
+  if(l != 0)
+  {
+    if (l == 30) //Numero
+    {
+      escribirSPI(65);
+      unsigned long r = l;
+      while( r == 30 || r == 0)
+      {
+        delay(200);
+        r = leerSPI();
+      }
+      Keyboard.print(nummap[r]);
+      escribirSPI(0);
+      Serial1.println(nummap[r]);
+    }
+    else
+    {
+     Keyboard.print(charmap[l]);
+     Serial.println(charmap[l]);
+     char c = charmap[l];
+     Serial1.println(c);
+    }
+  }
+  delay(210);
+}
+
+void iniciarmapa()
+{
+  nummap[96]='2';
+  charmap[32]='a';
+  charmap[96]='b';
+  charmap[36]='c';
+  charmap[38]='d';
+  charmap[34]='e';
+  charmap[100]='f';
+  charmap[102]='g';
+  charmap[98]='h';
+  charmap[68]='i';
+  charmap[70]='j';
+  charmap[162]='k';
+  charmap[224]='l';
+  charmap[164]='m';
+  charmap[166]='n';
+  charmap[103]='ñ';
+  charmap[162]='o';
+  charmap[228]='p';
+  charmap[230]='q';
+  charmap[226]='r';
+  charmap[196]='s';
+  charmap[198]='t';
+  charmap[161]='u';
+  charmap[225]='v';
+  charmap[71]='w';
+  charmap[165]='x';
+  charmap[167]='y';
+  charmap[163]='z';
+  charmap[128]='.';
+  charmap[64]=',';
+  charmap[65]='?';
+  charmap[192]=';';
+  charmap[67]=':';
+  charmap[132]='/';
+  charmap[194]='!';
+  charmap[66]='-';
+  charmap[130]='*';
+  charmap[193]='"';
+  charmap[97]='(';
+  charmap[134]=')';
+  charmap[101]='ü';
+  charmap[199]='ú';
+  charmap[133]='ó';
+  charmap[131]='í';
+  charmap[197]='é';
+  charmap[227]='á';
+  charmap[229]='&';
+  charmap[129]='_';
+  charmap[24]=KEY_RETURN;
+  charmap[16]=KEY_BACKSPACE;
+}
+
---
+++ funciones_base.ino
@@ -0,0 +1,31 @@
+//ESTAS FUNCIONES SON BASE Y NO DEBEN SER MODIFICADAS.
+
+unsigned long leerSPI()
+{
+  unsigned long valor = 0;
+  SPI.setBitOrder(LSBFIRST);
+  digitalWrite(SSB,LOW);
+  for(int i = 0 ; i < BYTES; i++)
+  {  
+    valor += (unsigned long)(SPI.transfer(0x00)) << 8*i; // = (Byte1 * << 8*0) + (Byte2 * 8*1) + (Byte3 * 8*2)
+  }
+  digitalWrite(SSB,HIGH);
+  return valor;  
+}
+
+void escribirSPI(unsigned long num)
+{
+  byte dato;
+  SPI.setBitOrder(LSBFIRST);
+  digitalWrite(SSL,LOW);
+  //Descomponemos el numero en sus bytes para enviarlos de a uno.
+  //al final de la funcion num siempre valdrá 0.
+  for(int i = 0 ; i < BYTES; i++)
+  { 
+    dato = num%256;
+    num /= 256; 
+    SPI.transfer(dato);
+  }
+  digitalWrite(SSL,HIGH);  
+} 
+
